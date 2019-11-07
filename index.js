0d358c1129ff7cce983d7992c5ed3a1047901554
Lucian Amos <locamos@gmail.com>
2019-11-07 13:59:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ function logWhisper(string) {
 }
 
 function sayhiToGrandma(string) {
-  if(string.toLowerCase()===lowercase){
+  if(string===lowercase){
   return("I can't hear you!")
+  }else if(string===uppercase){
+    return("YES INDEED!")
+  }else if(string=`I love you, Grandma.`){
+    return("I love you, too.")
   }
 }
